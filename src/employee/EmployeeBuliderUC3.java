a5a7080dca8cc7711e38beb9a09e3e7911d0d299
Prachi1808 <prachimohanty.1808@gmail.com>
2022-02-15 17:28:16 +0530

    Add employee wage computation problem

---
+++ src/employee/EmployeeBuliderUC3.java
@@ -0,0 +1,25 @@
+package employee;
+
+public class EmployeeBuliderUC3 {
+
+	public static void main(String[] args) {
+		
+			// Constants
+		
+		    int IS_PART_TIME =1;
+			int IS_FULL_TIME = 3;
+		    int EMP_RATE_PER_HOUR = 20;
+			//variables
+			int empHrs= 0;
+			int empWage = 0;
+			//Computation
+			double empCheck = Math.floor(Math.random() * 10) % 3;
+			if (empCheck == IS_FULL_TIME)
+	         empHrs =8;
+			else if (empCheck == IS_PART_TIME)
+				empHrs =4;
+			empWage = empHrs * EMP_RATE_PER_HOUR;
+			System.out.println("Emp Wage : " + empWage);
+		}
+
+	}
